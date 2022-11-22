e304d547e3f30a43813f95daf128943393682168
AzureAD\MarkusAltin <markus.altin@xlent.se>
2022-11-22 15:38:27 +0100

    HH

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 export const Header = () => {
   return (
     <div className='header'>
-        <img className='wave-icon' src='../images/wave.png' alt="" />
+        <img className='wave-icon' src='badprognoser/images/wave.png' alt="" />
         <h1 className='app-heading'> Badprognoser</h1>
-        <img className='surfboard-icon' src="../images/surfboard.png" alt="" />
+        <img className='surfboard-icon' src="badprognoser/images/surfboard.png" alt="" />
     </div>
   )
 }
